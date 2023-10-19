31547faf60fa4856d631565ae057b21be46dbcb5
juadacaf <128726718+juadacaf@users.noreply.github.com>
2023-10-19 07:36:27 -0500

    edicion5

--- 20231019a/funciones.php
+++ 20231019a/funciones.php
@@ -1,12 +1,13 @@
 <?php
-//conecta a una base de datos
-$conexion = mysqli_connect('localhost','root','','db_ejemplo');
+
     //Es una impresión que se mostrara en los archivos que esten incluidos 
     echo "Hola=";
     function consulta(){
         //inicializa la variable
         $salida=0;
         //calcula el area de un triangulo 
+        //conecta a una base de datos
+        $conexion = mysqli_connect('localhost','root','','db_ejemplo');
         $salida=10*2/2;
 
         //retorna la función
