0b0446419ed67d7fb2d357316e48a9450bcb1bfd
juadacaf <128726718+juadacaf@users.noreply.github.com>
2023-10-19 09:10:02 -0500

    edicion8

--- 20231019a/funciones.php
+++ 20231019a/funciones.php
@@ -51,33 +51,9 @@
     }
 
     
+   
+
     function calculo2(){
-        //inicializa la variable
-        $salida=0;
-        //conecta a una base de datos
-        $conexion = mysqli_connect('localhost','root','','db_ejemplo');
-        //es una suma de una base de datos 
-        $sql=" SELECT 10 as n1, 20 as n2 ";
-        
-        //da el resultado 
-        $imprimir= $conexion->query("$sql");
-
-        //es para que muestre el resultado
-        while($fila = $imprimir->fetch_array())
-        {
-        //imprime en el navegador la variable 'SUMA'
-        $salida += $fila['0'];
-        $salida += $fila['1'];
-        }
-        
-        //cierra la conexion
-       $conexion->close();
-
-       //retorna la función
-        return $salida;
-    }
-
-    function calculo3(){
         //inicializa la variable
         $salida=0;
         //conecta a una base de datos
@@ -102,7 +78,37 @@
        //retorna la función
         return $salida;
     }
+    function calculo3(){
+        //inicializa la variable
+        $salida="";
+        //conecta a una base de datos
+        $conexion = mysqli_connect('localhost','root','','db_ejemplo');
+        //es una suma de una base de datos 
+        $sql=" SELECT 17 as edad ";
+        
+        //da el resultado 
+        $imprimir= $conexion->query("$sql");
 
+        //es para que muestre el resultado
+        while($fila = $imprimir->fetch_assoc())
+        {
+            //si es mayor de edad
+            if ( $fila ['edad']>=18){
+                //muestra si es mayor de edad
+                $salida .= $fila['edad']. "ERES MAYOR DE EDAD:)";
+            //si es menor de edad
+            }else{
+            //muestra si es menor de edad
+            $salida .= $fila['edad']."ERES UN POLLITO:(";
+        }
+        }
+        
+        //cierra la conexion
+       $conexion->close();
+
+       //retorna la función
+        return $salida;
+    }
 
 
 
